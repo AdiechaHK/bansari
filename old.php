c712a7536aea512b75ecaa3713379cca26ad1740
Harikrushna <adiechahari@gmail.com>
2024-02-17 13:01:22 +0530

    new website

---
+++ old.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bansari Tech</title>
+    <link rel="icon" type="image/x-icon" href="/BT-logo-square.png">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="">
+    <div class="flex h-screen justify-center items-center">
+        <div class="text-center">
+            <img src="BT-logo.png" alt="Bansari Tech logo">
+            <div class="text-center p-2">
+                <h1 class="text-2xl text-gray-500">Software Consultancy</h1>
+                <hr class="my-2">
+                <h2 class="text-xl underline">Harikrushna V. Adiecha</h2>
+                <div class="border my-5 p-1 rounded border-gray-400 shadow-xl">
+                    <span class="absolute rounded-md border-l border-r border-gray-400 -mt-4 bg-white px-2 -ml-2">✉️</span>
+                    <div class="py-2">
+                        <a class="w-full" href="mailto:hk@bansari.tech">hk@bansari.tech</a>
+                    </div>
+                </div>
+                <div class="border mt-5 p-1 rounded border-gray-400 shadow-xl">
+                    <span class="absolute rounded-md border-l border-r border-gray-400 -mt-4 bg-white px-2 -ml-2">📞</span>
+                    <div class="py-2">
+                        <a href="tel:+919033319723">+91-903-331-9723</a>
+                    </div>
+                </div>
+                <div class="border mt-5 p-1 rounded border-gray-400 shadow-xl">
+                    <span class="absolute rounded-md border-l border-r border-gray-400 -mt-4 bg-white px-2 -ml-2">📌</span>
+                    <p class="text-lg pt-2"><strong>A-618</strong> The One World</p>
+                    <p>Near Ayodhya Cholk, 150 Ft Ring Road</p>
+                    <p class="pb-2">Rajkot, Gujrat, India - 360005</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
